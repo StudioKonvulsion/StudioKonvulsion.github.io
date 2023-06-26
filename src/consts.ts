f9a09ae47a511eb184ae156f32824eda96c795c2
StudioKonvulsion <studiokonvulsion@gmail.com>
2023-06-26 12:19:16 +0100

    update website information

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'S.M.A.R.T. FOUNDATION';
+export const SITE_DESCRIPTION = 'A website dedicated to looking after your eye health when using electronic devices';
+export const HOMEPAGE_URL = 'https://www.screenrisk.com';
