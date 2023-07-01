b93ab95d366a13d0333711e812435803b3369562
StudioKonvulsion <studiokonvulsion@gmail.com>
2023-07-01 13:45:02 +0100

    Info added

--- src/consts.ts
+++ src/consts.ts
@@ -3,5 +3,5 @@
 
 export const SITE_TITLE = 'S.M.A.R.T. Foundation';
 export const SITE_DESCRIPTION = 'A website dedicated to looking after your eye health when using electronic devices';
-export const HOMEPAGE_URL = 'https://www.studiokonvulsion.com';
+export const HOMEPAGE_URL = '/';
 export const CREATOR_URL = 'https://www.studiokonvulsion.com';
