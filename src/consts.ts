940bcfefa422c17cd76077d47d9e3b7e463eacd5
StudioKonvulsion <studiokonvulsion@gmail.com>
2024-03-19 13:24:39 +0000

    Updates to font and 2 pages added

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'S.M.A.R.T. Foundation';
-export const SITE_DESCRIPTION = 'A website dedicated to looking after your eye health when using electronic devices';
+export const SITE_DESCRIPTION = 'A website dedicated to looking after your health while using electronic devices';
 export const HOMEPAGE_URL = '/';
 export const CREATOR_URL = 'https://www.studiokonvulsion.com';
